2ceed6945835873c861718fd51742e46f3c91834
Neil <chenweining960227@gmail.com>
2023-06-26 17:19:37 +0800

    Updates

--- assets/footertext-2fcd6343.js
+++
@@ -1,1 +0,0 @@
-import{D as a,r,o as n,j as c,w as _,d as l,t as p,f as u}from"./index-484b5e78.js";const d={__name:"footertext",setup(f){const t=a(),e=()=>{t.push("/")},o=r("Neil Chen % cd ↵");return(i,m)=>{const s=u;return n(),c(s,{class:"m-13"},{default:_(()=>[l("span",{class:"mr-100 btnani textBlack ft w-[100%]",onClick:e},p(o.value),1)]),_:1})}}};export{d as _};
