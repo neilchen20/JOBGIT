af661d29057434c98e0a49b657437252b1b8a31a
Neil <chenweining960227@gmail.com>
2024-06-02 16:43:48 +0800

    home page update

--- nlcn/src/router/index.js
+++ nlcn/src/router/index.js
@@ -1,9 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+const routers = [
     {
       path: '/',
       name: 'index',
@@ -35,9 +33,29 @@ const router = createRouter({
       path: '/apptest',
       name: 'apptest',
       component: () => import ('../views/apptest.vue')
-    }
-
-  ]
+    },
+    {
+    path: "/404",
+    // alias: "*",
+    name: "notFound",
+    component: () => import("../views/NotFound.vue"),
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404",
+  },
+]
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes: routers,
 })
+
+router.beforeEach((to, from, next) => {
+  if (!to.matched.length) {
+    next('/404');
+  } else {
+    next();
+  }
+});
 
 export default router
